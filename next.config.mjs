b13e9eb909427800734cd83112370918dea2b449
etwicaksono <ekoteguhwicaksono@gmail.com>
2024-08-18 06:17:07 +0700

    Remove /out from .gitignore

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+   output: 'export',
+   basePath: '/etwicaksono.github.io',
+   assetPrefix: '/etwicaksono.github.io/',
+};
 
 export default nextConfig;
